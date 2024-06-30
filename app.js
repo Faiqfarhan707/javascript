e99d9006c6f934ca2ecf7b61688193dc43750937
Faiqfarhan707 <faiq365@gmail.com>
2024-06-30 14:55:22 +0500

    assinment operator

---
+++ app.js
@@ -0,0 +1,24 @@
+// console.log("hello js")
+
+// console.log(2+2)
+
+// console.log("2"+"3")
+
+// var username = "faiq"
+
+// console.log(username)
+
+// var password = 'pass123'
+// console.log(password)
+
+// alert('load')
+
+
+var num1 = 20;
+// num1 = num1 + 5
+// num1 += 5
+// num1-= 5
+// num1 /= 5
+num1 *= 5
+
+console.log(num1)
